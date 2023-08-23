b234b825886dfac998182a578d181e9f5836b038
Kay Nguyen <kaynguyen.work@gmail.com>
2023-08-24 00:07:56 +0700

    add max withdraw

--- tools/withdrawERC20.js
+++ tools/withdrawERC20.js
@@ -24,6 +24,7 @@ async function withdrawTo(address_) {
         console.log("Owner address is not you!")
         exit()
     }
+    let cacheWithdraw = 0
     for (let index = 0; index < myAcc.length; index++) {
         let isContract = await web3.eth.getStorageAt(myAcc[index][0])
         if (!Number(isContract) || myAcc[index][0] == address_ || myAcc[index][1] == '_F_E_A') { continue }
@@ -31,6 +32,8 @@ async function withdrawTo(address_) {
         console.log(myAcc[index][0])
         console.log(balanceSC / 10 ** 18)
         if (balanceSC / 10 ** 18 > 10) {
+            cacheWithdraw += balanceSC / 10 ** 18
+            balanceSC = ((cacheWithdraw - maxWithdraw) * 10 ** 18).toFixed()
             try {
                 const contractAddress = new web3.eth.Contract(abi, myAcc[index][0]);
                 let encoded = await contractAddress.methods.transferERC20('0xe9e7cea3dedca5984780bafc599bd69add087d56', address_, balanceSC).encodeABI();
@@ -47,8 +50,13 @@ async function withdrawTo(address_) {
             } catch (error) {
                 console.log("Encode Fail", error)
             }
+            if (cacheWithdraw > maxWithdraw) {
+                console.log("Done withdraw max", maxWithdraw)
+            }
         }
     }
 }
 
-withdrawTo('0x7457aCc0f158734CFF4d9375efC90bc3082bB256')
+const maxWithdraw = 50
+
+withdrawTo('0x11119D51e2Ff85D5353ABf499Fe63bE3344c0000')
