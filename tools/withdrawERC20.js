ede22314c9ce64e8d5167af97f5cd834ec661699
Kay Nguyen <kaynguyen.work@gmail.com>
2023-08-24 03:20:29 +0700

    update withdraw erc20

--- tools/withdrawERC20.js
+++ tools/withdrawERC20.js
@@ -32,7 +32,7 @@ async function withdrawTo(address_) {
     let cacheWithdraw = 0
     for (let index = 0; index < myAcc.length; index++) {
         let isContract = await web3.eth.getStorageAt(myAcc[index][0])
-        if (!Number(isContract) || myAcc[index][0] == address_ || myAcc[index][1] == '_F_E_A') { continue }
+        if (!Number(isContract) || myAcc[index][0] == address_) { continue }
         let balanceSC = await contractBUSD.methods.balanceOf(myAcc[index][0]).call()
         console.log(myAcc[index][0])
         console.log(balanceSC / 10 ** 18)
