056491361b72ae07bd6322c9f9a9e81084245384
Kay Nguyen <kaynguyen.work@gmail.com>
2023-10-03 16:09:54 +0700

    update

--- tools/create.js
+++ tools/create.js
@@ -358,6 +358,6 @@ priceList = [];
 ids = [];
 const minChange = 0.001;
 var accSell = "";
-value = 67; // without rare and epic
+value = 110; // without rare and epic
 
-createBatch(3.001, 1000000, "", "_7_3_A");
+createBatch(3.001, 1000000, "", "_1_D_f");
