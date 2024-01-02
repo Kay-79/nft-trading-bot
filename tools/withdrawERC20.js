eff70413a76945b7f3f3457382c3ff2d42089a4a
Kay Nguyen <kaynguyen.work@gmail.com>
2024-01-03 01:35:19 +0700

    change acc

--- tools/withdrawERC20.js
+++ tools/withdrawERC20.js
@@ -80,4 +80,4 @@ async function withdrawTo(address_) {
 const minWithdraw = 10;
 const maxWithdraw = 500;
 
-withdrawTo("0x0000a7514Bc1e72058B709A713d20c1fE68b7777");
+withdrawTo("0x88888dF23F9554e4B043B00E1F4AfB39Fc078888");
