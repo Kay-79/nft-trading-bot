74ccdb1ebf2c7f0df339e9b64d1f3bd41d339765
Kay Nguyen <134670945+KayNguyenDev@users.noreply.github.com>
2024-01-29 11:33:22 +0700

    Update bid.js

--- tools/bid.js
+++ tools/bid.js
@@ -220,7 +220,7 @@ async function setup(Private_Key_) {
                         await sleep(1000); //sleep to avoid pending hash
                         timeSendReal = await web3.eth.getTransaction(checkHashEach);
                         timeSendReal = await web3.eth.getBlock(timeSendReal.blockNumber);
-                        timeSendReal = timeSendReal.timestamp - 1;
+                        timeSendReal = timeSendReal.timestamp;
                         console.log("timeStampFail:", timeSendReal);
                     }
                     if (
