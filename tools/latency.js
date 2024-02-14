2be116090ab242a7becef0bbc6b6fff5aa8239c6
Kay Nguyen <kaynguyen.work@gmail.com>
2024-02-14 10:38:43 +0700

    Add latency check script to package.json

---
+++ tools/latency.js
@@ -0,0 +1,17 @@
+const Web3 = require("web3");
+const configJson = require("../config/config");
+const { exit } = require("process");
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const web3a = new Web3(new Web3.providers.HttpProvider(configJson.rpcs.bid));
+const checkLatency = async () => {
+    try {
+        const latency = await web3.eth.getBlockNumber();
+        console.log(`Latency private: ${latency}`);
+        const latency2 = await web3.eth.getBlockNumber();
+        console.log(`Latency public: ${latency2}`);
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+    exit(0);
+};
+checkLatency();
