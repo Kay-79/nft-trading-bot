9d6fd5dbb47740dc3766ba9eadb0b97631a0fa3e
Kay Nguyen <kaynguyen.work@gmail.com>
2024-02-14 12:45:44 +0700

    Update latency.js to check latency on both public and private nodes

--- tools/latency.js
+++ tools/latency.js
@@ -1,14 +1,18 @@
 const Web3 = require("web3");
 const configJson = require("../config/config");
 const { exit } = require("process");
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 const web3a = new Web3(new Web3.providers.HttpProvider(configJson.rpcs.bid));
 const checkLatency = async () => {
     try {
-        const latency2 = await web3.eth.getBlockNumber();
+        const latency2 = await web3a.eth.getBlockNumber();
         console.log(`Latency public: ${latency2}`);
-        const latency = await web3.eth.getBlockNumber();
-        console.log(`Latency private: ${latency}`);
+        try {
+            const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+            const latency = await web3.eth.getBlockNumber();
+            console.log(`Latency private: ${latency}`);
+        } catch (error) {
+            console.log("Check on private node failed");
+        }
     } catch (error) {
         console.log("Error: ", error);
     }
