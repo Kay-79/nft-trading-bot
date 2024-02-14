abea66b7e5d4d72bb6e3ff4a7c24b215c1c43309
Kay Nguyen <kaynguyen.work@gmail.com>
2024-02-14 10:40:50 +0700

    Refactor latency.js to improve code structure and logging

--- tools/latency.js
+++ tools/latency.js
@@ -5,10 +5,10 @@ const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 const web3a = new Web3(new Web3.providers.HttpProvider(configJson.rpcs.bid));
 const checkLatency = async () => {
     try {
-        const latency = await web3.eth.getBlockNumber();
-        console.log(`Latency private: ${latency}`);
         const latency2 = await web3.eth.getBlockNumber();
         console.log(`Latency public: ${latency2}`);
+        const latency = await web3.eth.getBlockNumber();
+        console.log(`Latency private: ${latency}`);
     } catch (error) {
         console.log("Error: ", error);
     }
