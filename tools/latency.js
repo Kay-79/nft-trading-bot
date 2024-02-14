b1b82037ae4cf2dde5313029eba56af90b0c2953
Kay Linux <kaynguyen.work@gmail.com>
2024-02-14 16:29:28 +0700

    refactor

--- tools/latency.js
+++ tools/latency.js
@@ -5,7 +5,7 @@ const web3a = new Web3(new Web3.providers.HttpProvider(configJson.rpcs.bid));
 const checkLatency = async () => {
     try {
         const latency2 = await web3a.eth.getBlockNumber();
-        console.log(`Latency public: ${latency2}`);
+        console.log(`Latency public : ${latency2}`);
         try {
             const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
             const latency = await web3.eth.getBlockNumber();
