49c417fb6fe3fe106484e5134de093b31d735ad5
Kay Nguyen <kaynguyen.work@gmail.com>
2024-02-21 01:46:44 +0700

    Update minBid gas price in config.js

--- config/config.js
+++ config/config.js
@@ -42,7 +42,7 @@ const config = {
     minDecreasePrice: 0.2,
     amountChange: 1,
     gasPrices: {
-        minBid: 3.1,
+        minBid: 3.45,
         create: 1.0001,
         change: 1.0001,
     },
