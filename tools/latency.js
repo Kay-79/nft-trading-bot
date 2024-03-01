593382d9495945f871814054b2913103e0f796a7
Kay Nguyen <kaynguyen.work@gmail.com>
2024-03-01 17:21:27 +0700

    enhance: check latency

--- tools/latency.js
+++ tools/latency.js
@@ -2,20 +2,26 @@ const Web3 = require("web3");
 const configJson = require("../config/config");
 const { exit } = require("process");
 const web3a = new Web3(new Web3.providers.HttpProvider("https://bsc-dataseed3.bnbchain.org"));
+try {
+    const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+} catch (error) {}
+const { sleep } = require("../utils/common/sleep");
 const checkLatency = async () => {
     try {
-        const latency2 = await web3a.eth.getBlockNumber();
-        console.log(`Latency public : ${latency2}`);
+        web3a.eth.getBlockNumber().then((latencyPublic) => {
+            console.log(`Latency public : ${latencyPublic}`);
+        });
         try {
-            const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-            const latency = await web3.eth.getBlockNumber();
-            console.log(`Latency private: ${latency}`);
+            web3.eth.getBlockNumber().then((latencyPrivate) => {
+                console.log(`Latency private: ${latencyPrivate}`);
+            });
         } catch (error) {
             console.log("Check on private node failed");
         }
     } catch (error) {
         console.log("Error: ", error);
     }
+    await sleep(3000);
     exit(0);
 };
 checkLatency();
