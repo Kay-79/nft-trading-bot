bd1809281d88291764cd61b8a11f9643bb11ddd6
Kay Nguyen <kaynguyen.work@gmail.com>
2024-03-05 23:01:46 +0700

    chang acc

--- config/config.js
+++ config/config.js
@@ -65,7 +65,7 @@ const config = {
         minRare: 14,
     },
     timeBid: 132.5,
-    accBuy: "0xfa11AA3953B46c12dC1fB5c880912A80BF52203A",
+    accBuy: "0x891016f99BA622F8556bE12B4EA336157aA6cb20",
 };
 
 module.exports = config;
