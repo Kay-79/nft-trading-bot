b6fff96f20d9e4ae2e01987a1d6576fc85bad826
Kay Nguyen <kaynguyen.work@gmail.com>
2024-03-09 13:51:03 +0700

    push to sync

--- tools/transfer.js
+++ tools/transfer.js
@@ -81,6 +81,6 @@ async function transfer(address_) {
 }
 
 const minTransfer = 1;
-const maxTransfer = 500;
+const maxTransfer = 9999;
 
-transfer("0xfa11AA3953B46c12dC1fB5c880912A80BF52203A");
+transfer("0x891016f99BA622F8556bE12B4EA336157aA6cb20");
