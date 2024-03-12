79c7c407484bec11d73b6d230cadc0d049c4d9fc
Kay Nguyen <kaynguyen.work@gmail.com>
2024-03-12 17:21:17 +0700

    fix: redundant "\n" and nowBlock++ to avoid fail create

--- utils/create/updateZeroBlock.js
+++ utils/create/updateZeroBlock.js
@@ -6,20 +6,24 @@ const getAmountUnlist = require("../common/getAmountUnlist");
 const { sleep } = require("../common/sleep");
 const myaccs = configJson.myAcc;
 const Web3 = require("web3");
-const web3 = new Web3(configJson.rpcs.change);
-// const web3 = new Web3("https://bsc-dataseed3.bnbchain.org");
+// const web3 = new Web3(configJson.rpcs.change);
+const web3 = new Web3("https://bsc-dataseed1.binance.org:443");
 const updateZeroBlock = async () => {
     let newDataZero = "";
     let contracts = myaccs.map((e) => e[0]);
     let nowBlock = await web3.eth.getBlockNumber();
-    // nowBlock = nowBlock.data.result;
+    nowBlock++;
     console.log(nowBlock);
     let lastContract = "";
     try {
         const data = fs.readFileSync("./data/zeroBlock.csv", "utf8");
         const dataArr = data.split("\n");
         for (let i = 0; i < dataArr.length; i++) {
+            if (dataArr[i] === "") {
+                continue;
+            }
             let dataAddress = dataArr[i].split("|");
+            // console.log(dataAddress);
             lastContract = dataAddress[0];
             const amountUnlist = await getAmountUnlist(dataAddress[0]);
             if (amountUnlist === "0") {
@@ -37,7 +41,6 @@ const updateZeroBlock = async () => {
                 newDataZero += `${contracts[i]}|${nowBlock}\n`;
             }
         }
-        // last '\n' remove
         newDataZero = newDataZero.trim();
         fs.writeFileSync("./data/zeroBlock.csv", newDataZero);
         await sleep(1000);
