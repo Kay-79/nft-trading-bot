c0fb2ac56cdddda2e04e1884dac5f59f5e2e2b46
Kay Nguyen <kaynguyen.work@gmail.com>
2024-04-03 10:34:32 +0700

    up time bid

--- tools/bid.js
+++ tools/bid.js
@@ -196,7 +196,7 @@ async function setup(Private_Key_) {
                                 nowBlock = await web3rpc.eth.getBlockNumber();
                                 if (blockCreate + 38 <= nowBlock) {
                                     //control time to send here
-                                    await sleep(2000);
+                                    await sleep(2150);
                                     isFrontRun = true;
                                     break;
                                 }
