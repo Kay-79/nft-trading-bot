b9ef5479faf3860a20533d0de6c68e11e0539f2e
Kay Nguyen <kaynguyen.work@gmail.com>
2024-04-12 18:34:57 +0700

    fix after 5 day cant list

--- utils/create/checkMomosUnlistPrivateNode.js
+++ utils/create/checkMomosUnlistPrivateNode.js
@@ -116,7 +116,7 @@ const checkMomosUnlistPrivateNode = async (addressCheck, boolSaveInventory) => {
     const hexAddress = `0x000000000000000000000000${addressCheck.toLowerCase().slice(2)}`;
     let nowBlock = await getBlockByTime(web3, (Date.now() / 1000 - 10).toFixed(0));
     console.log(nowBlock);
-    if (nowBlock - momoStorage[addressCheck]["block"] < 144000) {
+    if (nowBlock - momoStorage[addressCheck]["block"] < 144000 && boolSaveInventory) {
         console.log(`${addressCheck} wait for update after 5 days`);
         return;
     }
