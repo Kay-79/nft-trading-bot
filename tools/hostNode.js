59777f488f104fed4f50890942ca22d409990c81
Kay Nguyen <kaynguyen.work@gmail.com>
2024-07-08 00:00:49 +0700

    enhance: fix crash telegram bot

--- tools/hostNode.js
+++ tools/hostNode.js
@@ -3,7 +3,23 @@ const Web3 = require("web3");
 const config = require("../config/config");
 require("dotenv").config();
 const token = process.env.api_telegram.replace("bot", "");
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, {
+    polling: {
+        interval: 1000,
+        autoStart: true,
+        params: {
+            timeout: 10,
+        },
+    },
+});
+
+bot.on("polling_error", (error) => {
+    console.error("Polling error:", error);
+});
+
+bot.on("webhook_error", (error) => {
+    console.error("Webhook error:", error);
+});
 
 bot.onText(/\/status/, async (msg) => {
     const chatId = msg.chat.id;
@@ -12,12 +28,16 @@ bot.onText(/\/status/, async (msg) => {
         const blockNumber = await web3.eth.getBlockNumber();
         const amountPeer = await web3.eth.net.getPeerCount();
         if (blockNumber > 0) {
-            bot.sendMessage(chatId, "Block number: " + blockNumber + "\nAmount peer: " + amountPeer);
-        } else {
-            const isSyncing = await web3.eth.isSyncing();
             bot.sendMessage(
                 chatId,
-                `Syncing:\nstartingBlock: ${isSyncing.startingBlock}\ncurrentBlock: ${isSyncing.currentBlock}\nhighestBlock: ${isSyncing.highestBlock}\nknownStates: ${isSyncing.knownStates}\npulledStates: ${isSyncing.pulledStates}`
+                "Block number: " + blockNumber + "\nAmount peer: " + amountPeer
+            );
+        } else {
+            const isSyncing = await web3.eth.isSyncing();
+            const amountPeer = await web3.eth.net.getPeerCount();
+            bot.sendMessage(
+                chatId,
+                `Syncing:\nstartingBlock: ${isSyncing.startingBlock}\ncurrentBlock: ${isSyncing.currentBlock}\nhighestBlock: ${isSyncing.highestBlock}\nAmount peer: ${amountPeer}`
             );
         }
     } catch (error) {
