d0191b3e31ffe69a4eb8824aa0ba7cb7075bb840
Kay Nguyen <kaynguyen.work@gmail.com>
2024-09-07 14:03:11 +0700

    rm momoUnList field

--- contracts/implement/Implement.sol
+++ contracts/implement/Implement.sol
@@ -6,19 +6,15 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract Bid is OwnableUpgradeable {
     address public addressMP = 0xcB0CffC2B12739D4BE791b8aF7fbf49bc1d6a8c2;
     address public addressMomo = 0x3bD6a582698ECCf6822dB08141818A1a8512c68D;
-    uint256 public amountUnList;
     address public changer = 0x11119D51e2Ff85D5353ABf499Fe63bE3344c0000;
 
     modifier onlyChanger() {
-        require(
-            msg.sender == changer,
-            "Only the contract changer can call this function."
-        );
+        require(msg.sender == changer, "Only the contract changer can call this function.");
         _;
     }
 
     function initialize() public initializer {
-        __Ownable_init();
+        __Ownable_init(msg.sender);
     }
 
     receive() external payable {}
@@ -31,32 +27,18 @@ contract Bid is OwnableUpgradeable {
         addressMP = address(newMP_);
     }
 
-    function changeAmountUnList(uint256 newAmount) public onlyOwner {
-        amountUnList = newAmount;
-    }
-
-    function withdraw(
-        uint256 amount,
-        address payable destAddr
-    ) public onlyOwner {
+    function withdraw(uint256 amount, address payable destAddr) public onlyOwner {
         destAddr.transfer(amount);
     }
 
-    function transferERC20(
-        IERC20 token,
-        address to,
-        uint256 amount
-    ) public onlyOwner {
+    function transferERC20(IERC20 token, address to, uint256 amount) public onlyOwner {
         uint256 erc20balance = token.balanceOf(address(this));
         require(amount <= erc20balance, "Balance is low");
         token.transfer(to, amount);
     }
 
-    function approve(
-        address spender,
-        address token
-    ) external payable onlyOwner {
-        (bool success, ) = token.call{gas: gasleft(), value: msg.value}(
+    function approve(address spender, address token) external payable onlyOwner {
+        (bool success, ) = token.call{ gas: gasleft(), value: msg.value }(
             abi.encodeWithSignature(
                 "approve(address,uint256)",
                 spender,
@@ -72,10 +54,8 @@ contract Bid is OwnableUpgradeable {
         address auctor_,
         uint256 index_,
         uint256 startTime_,
-        uint256 price_,
-        uint256 amount_ //must have to count momo
+        uint256 price_
     ) external payable onlyOwner {
-        amountUnList += amount_;
         (bool success, bytes memory returnData) = addressMP.call{
             gas: gasleft(),
             value: msg.value
@@ -123,7 +103,6 @@ contract Bid is OwnableUpgradeable {
         uint256[] memory ids_,
         uint256[] memory prices1155_
     ) external payable onlyChanger {
-        amountUnList -= (ids_.length + prices721_.length);
         (bool success, bytes memory returnData) = addressMP.call{
             gas: gasleft(),
             value: msg.value
@@ -143,7 +122,6 @@ contract Bid is OwnableUpgradeable {
     }
 
     function cancelAuction(uint256 index_) external payable onlyChanger {
-        amountUnList += 1; //add
         (bool success, bytes memory returnData) = addressMP.call{
             gas: gasleft(),
             value: msg.value
