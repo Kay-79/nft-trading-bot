8fa01de73125dbf8028f9ee92784c86911dee537
Kay Nguyen <kaynguyen.work@gmail.com>
2024-11-16 14:56:09 +0700

    update version contract

--- contracts/wallets/Wallet.sol
+++ contracts/wallets/Wallet.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: None
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -15,10 +15,7 @@ contract Wallet is OwnableUpgradeable {
         return address(this).balance;
     }
 
-    function withdraw(
-        uint256 amount,
-        address payable destAddr
-    ) public onlyOwner {
+    function withdraw(uint256 amount, address payable destAddr) public onlyOwner {
         uint256 myBalance = address(this).balance;
         require(amount <= myBalance, "Balance is low");
         destAddr.transfer(amount);
@@ -28,11 +25,7 @@ contract Wallet is OwnableUpgradeable {
         return token.balanceOf(address(this));
     }
 
-    function transferToken(
-        IERC20 token,
-        address to,
-        uint256 amount
-    ) public onlyOwner {
+    function transferToken(IERC20 token, address to, uint256 amount) public onlyOwner {
         uint256 erc20balance = token.balanceOf(address(this));
         require(amount <= erc20balance, "Balance is low");
         token.transfer(to, amount);
