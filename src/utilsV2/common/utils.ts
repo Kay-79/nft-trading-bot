0e7bae4bfd388c6c28b1b5f7703e629b8e4d050d
Kay Nguyen <kaynguyen.work@gmail.com>
2024-11-22 13:54:18 +0700

    update

--- src/utilsV2/common/utils.ts
+++ src/utilsV2/common/utils.ts
@@ -7,3 +7,7 @@ export const decodeFunctionData = (functionFragment: string, data: string) => {
 export const shortenAddress = (address: string, prefix = 6, suffix = 4) => {
     return `${address.slice(0, prefix)}...${address.slice(-suffix)}`;
 };
+
+export const addressTo32Bytes = (address: string) => {
+    return "0x" + address.slice(2, 42).padStart(64, "0").toLowerCase();
+};
