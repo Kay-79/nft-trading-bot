ef7d4e671a69a85d9c91da287d95a99148f8b025
Kay Nguyen <kaynguyen.work@gmail.com>
2024-11-23 17:54:01 +0700

    feat: check delay block bid

--- src/utilsV2/common/utils.ts
+++ src/utilsV2/common/utils.ts
@@ -1,4 +1,7 @@
+import { FunctionFragment } from "../../enum/enum";
 import { contractProvider } from "../../providers/contractProvider";
+import { ethersProvider } from "../../providers/ethersProvider";
+import { getBlockByTimestamp } from "../bid/utils";
 
 export const decodeFunctionData = (functionFragment: string, data: string) => {
     return contractProvider.interface.decodeFunctionData(functionFragment, data);
@@ -10,4 +13,20 @@ export const shortenAddress = (address: string, prefix = 6, suffix = 4) => {
 
 export const addressTo32Bytes = (address: string) => {
     return "0x" + address.slice(2, 42).padStart(64, "0").toLowerCase();
+};
+
+export const checkDelayBlockTransaction = async (txHash: string): Promise<number> => {
+    const tx = await ethersProvider.provider.getTransaction(txHash);
+    if (!tx) {
+        return -1;
+    }
+    let prams = [];
+    try {
+        prams = decodeFunctionData(FunctionFragment.BID, tx.data);
+    } catch (error) {
+        prams = decodeFunctionData(FunctionFragment.BID_BATCH, tx.data);
+    }
+    const tagetBlock = (await getBlockByTimestamp(prams[2])) + 40;
+    // console.log(prams[2]);
+    return tx.blockNumber ? tx.blockNumber - tagetBlock : -1;
 };
