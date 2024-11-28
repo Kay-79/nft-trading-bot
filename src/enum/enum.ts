ddba17dc82dd960fb25e65545756e4ee3cdb1a7b
Kay Nguyen <kaynguyen.work@gmail.com>
2024-11-29 02:39:55 +0700

    update sig selector

--- src/enum/enum.ts
+++ src/enum/enum.ts
@@ -20,5 +20,13 @@ export enum BidStatus {
 }
 
 export enum MomoSelector {
-    EARNED = "0x008cc262"
+    UNKNOWN_1 = "0x390c5f07", //unknow function selector
+    UNKNOWN_2 = "0xd1728859", //get time unknown
+    UNKNOWN_3 = "0x501bbaec", //get time unknown
+    EARNED = "0x008cc262",
+    OWNER_OF_TOKEN_ID = "0x0eb9441f",
+    TOKENS_OF_OWNER = "0x8462151c",
+    USER_HASH_RATE = "0x73776e05",
+    GET_TOKEN_ID_USER_BY_INDEX = "0x2f745c59",
+    TEST = "0x501bbaec"
 }
--- src/utilsV2/momo/utils.ts
+++ src/utilsV2/momo/utils.ts
@@ -1,7 +1,7 @@
 import { MOMO_ADDRESS } from "../../constants/constants";
 import { MomoSelector } from "../../enum/enum";
 import { ethersProvider } from "../../providers/ethersProvider";
-import { AbiCoder } from "ethers";
+import { AbiCoder, getAddress } from "ethers";
 
 const earned = async (userAddress: string) => {
     const abiCoder = new AbiCoder();
@@ -14,18 +14,69 @@ const earned = async (userAddress: string) => {
     return Number(result) / 10 ** 18;
 };
 
-const test = async (tokenId: string): Promise<string> => {
+const ownerOfTokenId = async (tokenId: string): Promise<string> => {
     const abiCoder = new AbiCoder();
     const encodedData = abiCoder.encode(["uint256"], [tokenId]);
-    const data = "0x0eb9441f" + encodedData.slice(2);
+    const data = MomoSelector.OWNER_OF_TOKEN_ID + encodedData.slice(2);
     const result = await ethersProvider.call({
         to: MOMO_ADDRESS,
         data: data
     });
-    return "0x" + result.slice(26);
+    return getAddress("0x" + result.slice(26));
+};
+
+const tokensOfOwner = async (address: string): Promise<string> => {
+    const abiCoder = new AbiCoder();
+    const encodedData = abiCoder.encode(["address"], [address]);
+    const data = MomoSelector.TOKENS_OF_OWNER + encodedData.slice(2);
+    const result = await ethersProvider.call({
+        to: MOMO_ADDRESS,
+        data: data
+    });
+    const decodedResult = abiCoder.decode(["uint256[]"], result);
+    const nfts721 = decodedResult[0].map((x: any) => x.toString());
+    return nfts721;
+};
+
+const userHashrate = async (address: string): Promise<number> => {
+    const abiCoder = new AbiCoder();
+    const encodedData = abiCoder.encode(["address"], [address]);
+    const data = MomoSelector.USER_HASH_RATE + encodedData.slice(2);
+    const result = await ethersProvider.call({
+        to: MOMO_ADDRESS,
+        data: data
+    });
+    return Number(result);
+};
+
+const getTokenIdUserByIndex = async (address: string, index: string): Promise<number> => {
+    const abiCoder = new AbiCoder();
+    const encodedData = abiCoder.encode(["address", "uint256"], [address, index]);
+    const data = MomoSelector.GET_TOKEN_ID_USER_BY_INDEX + encodedData.slice(2);
+    const result = await ethersProvider.call({
+        to: MOMO_ADDRESS,
+        data: data
+    });
+    return Number(result);
+};
+
+const test = async (n: string): Promise<number> => {
+    const abiCoder = new AbiCoder();
+    const encodedData = abiCoder.encode(["uint256"], [n]);
+    const data = MomoSelector.GET_TOKEN_ID_USER_BY_INDEX + encodedData.slice(2);
+    const result = await ethersProvider.call({
+        to: MOMO_ADDRESS,
+        data: data
+    });
+    return Number(result);
 };
 
 export const momoUtils = {
     earned,
+    ownerOfTokenId,
+    tokensOfOwner,
+    userHashrate,
+    getTimeUnknown1,
+    getTokenIdUserByIndex,
     test
 };
