c51229da1194eb38ed352feb154b3712acae46ed
Kay Nguyen <kaynguyen.work@gmail.com>
2024-12-11 18:16:41 +0700

    make tool crawling datasets

---
+++ src/AI/crawlingDatasets.ts
@@ -0,0 +1,35 @@
+import { TrainingData } from "types/AI/TrainingData";
+import fs from "fs";
+import { API_MOBOX } from "constants/constants";
+import axios from "axios";
+import { RecentSold } from "types/dtos/TopAuction.dto";
+import { preprocessRawData } from "./utils";
+import { sleep } from "utilsV2/common/sleep";
+
+export const crawlingDatasets = async () => {
+    let cacheTimeInSec = Date.now() / 1000;
+    const interval = 1800;
+    while (true) {
+        let datasets = fs.readFileSync("./src/AI/data/datasets.json", "utf-8");
+        let trainingDatasets: TrainingData[] = JSON.parse(datasets);
+        console.log("Crawling datasets...");
+        // Crawling datasets
+        const data = await axios.get(`${API_MOBOX}/auction/logs_new`);
+        const rawDatasets: RecentSold[] = data.data.list;
+        const finalDatasets = [];
+        for (const dataset of rawDatasets.reverse()) {
+            if (!dataset.tx || (dataset.crtime ?? 0) <= cacheTimeInSec) continue;
+            cacheTimeInSec = dataset.crtime ?? Date.now() / 1000;
+            finalDatasets.push(dataset);
+        }
+        if (finalDatasets.length === 0) {
+            console.log("No new datasets, waiting for", interval, "seconds...");
+            await sleep(interval);
+            continue;
+        }
+        trainingDatasets.push(...preprocessRawData(finalDatasets));
+        fs.writeFileSync("./src/AI/data/datasets.json", JSON.stringify(trainingDatasets));
+        console.log("Crawling datasets done, waiting for next", interval, "seconds...");
+        sleep(interval);
+    }
+};
