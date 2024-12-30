c1f37539e12f5071b3d8dcca70245c3495f06858
Kay Nguyen <kaynguyen.work@gmail.com>
2024-12-30 22:37:50 +0700

    feat: getOrderHistory

--- src/utilsV2/mp/utils.ts
+++ src/utilsV2/mp/utils.ts
@@ -3,6 +3,7 @@ import { MpSelector } from "../../enum/enum";
 import { ethersProvider } from "../../providers/ethersProvider";
 import { AbiCoder } from "ethers";
 import { OrderInfo } from "../../types/dtos/OrderInfo.dto";
+import { archiveProvider } from "providers/archiveProvider";
 
 const getListedMomos = async (user: string) => {
     const abiCoder = new AbiCoder();
@@ -50,7 +51,43 @@ const getOrder = async (user: string, index: string): Promise<OrderInfo> => {
     return orderInforesult;
 };
 
+const getOrderHistory = async (user: string, index: string, block: number): Promise<OrderInfo> => {
+    const abiCoder = new AbiCoder();
+    const encodedAddress = abiCoder.encode(["address", "uint256"], [user, index]);
+    const data = MpSelector.GET_ORDER + encodedAddress.slice(2);
+    const result = await archiveProvider.call({
+        to: MP_ADDRESS,
+        data: data,
+        blockTag: block
+    });
+    const decodeData = abiCoder.decode(
+        [
+            "uint256",
+            "uint256",
+            "uint256",
+            "uint256",
+            "uint256",
+            "uint256",
+            "uint256[]",
+            "uint256[]"
+        ],
+        result
+    );
+    let orderInforesult: OrderInfo = {
+        status: decodeData[0],
+        startPrice: decodeData[1],
+        endPrice: decodeData[2],
+        uptime: decodeData[3],
+        durationDays: decodeData[4],
+        tokenId: decodeData[5],
+        ids: decodeData[6],
+        amounts: decodeData[7]
+    };
+    return orderInforesult;
+};
+
 export const mpUtils = {
     getListedMomos,
-    getOrder
+    getOrder,
+    getOrderHistory
 };
