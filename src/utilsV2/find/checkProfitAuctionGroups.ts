19ac398d1f8e4ebf6426e1074981e7fddcbfa662
Kay Nguyen <kaynguyen.work@gmail.com>
2025-01-04 02:22:28 +0700

    update

--- src/utilsV2/find/checkProfitAuctionGroups.ts
+++ src/utilsV2/find/checkProfitAuctionGroups.ts
@@ -8,27 +8,28 @@ import {
     getProfitableBidAuctionsBundle
 } from "./utils";
 import { BidType } from "enum/enum";
+import { AuctionGroupDto } from "types/dtos/AuctionGroup.dto";
 
 export const checkProfitAuctionGroups = async (
-    auctions: AuctionDto[],
+    auctionGroups: AuctionGroupDto[],
     floorPrices: TierPrice,
     bnbPrice: number
 ): Promise<BidAuction[]> => {
-    if (!auctions || auctions.length === 0) {
+    if (!auctionGroups || auctionGroups.length === 0) {
         return [];
     }
     let profitableBidAuctionGroups: BidAuction[] = [];
     let normalAuctions: AuctionDto[] = [];
     let proAuctions: AuctionDto[] = [];
     let bundleAuctions: AuctionDto[] = [];
-    for (let i = 0; i < auctions.length; i++) {
-        const auction = auctions[i];
-        if (isProAuction(auction)) {
-            proAuctions.push(auction);
-        } else if (isBundleAuction(auction)) {
-            bundleAuctions.push(auction);
+    for (let i = 0; i < auctionGroups.length; i++) {
+        const auctionGroup = auctionGroups[i];
+        if (isProAuction(auctionGroup)) {
+            proAuctions.push(auctionGroup);
+        } else if (isBundleAuction(auctionGroup)) {
+            bundleAuctions.push(auctionGroup);
         } else {
-            normalAuctions.push(auction);
+            normalAuctions.push(auctionGroup);
         }
     }
     profitableBidAuctionGroups.push(
