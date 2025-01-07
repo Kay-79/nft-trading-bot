3ddd59ec8c9eb0ba56cf588e011c62e65e3874f0
Kay Nguyen <kaynguyen.work@gmail.com>
2025-01-07 22:50:09 +0700

    enhance apiPredict

--- src/AI/api/app.py
+++ src/AI/api/app.py
@@ -10,18 +10,24 @@ scaler = joblib.load(scaler_path)
 
 app = Flask(__name__)
 
+
 @app.route('/predict', methods=['GET'])
 def predict():
     try:
         input_data = request.args.getlist('input', type=float)
         if not input_data:
             return jsonify({"error": "Invalid input"}), 400
+        if len(input_data) != 4:
+            return jsonify({"error": "Invalid input length"}), 400
+        if input_data[2] not in [4, 6]:
+            return jsonify({"error": "Invalid input value"}), 400
         input_data = np.array(input_data).reshape(1, -1)
         input_data = scaler.transform(input_data)
         prediction = model.predict(input_data)
         return jsonify({"prediction": prediction.tolist()})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
