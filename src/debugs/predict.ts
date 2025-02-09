978c99ce952dc071011cefea823a7576e1bff2ff
Kay Nguyen <kaynguyen.work@gmail.com>
2025-02-09 16:25:28 +0700

    update rate profit AI

--- src/debugs/predict.ts
+++ src/debugs/predict.ts
@@ -2,10 +2,10 @@ import { predictModel } from "AI/utils";
 import { PredictMode } from "enum/enum";
 
 const getDatasetF = async () => {
-    const price = 0.0963;
-    const reward = 0.12718886956974754;
+    const price = 0.11125;
+    const reward = 0.1267;
     const timestamp = Math.floor(Date.now() / 1000);
-    const momoInfo = [620, 13785, 5, 34];
+    const momoInfo = [181, 1615, 4, 14];
     momoInfo.push(...[timestamp, price, reward]);
     await predictModel(
         momoInfo,
