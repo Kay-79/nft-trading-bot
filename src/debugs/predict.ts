86c8d7f16f1a4dec57e6f7a1171edc352d8d41bf
Kay Nguyen <kaynguyen.work@gmail.com>
2025-02-12 01:49:10 +0700

    update readme

--- src/debugs/predict.ts
+++ src/debugs/predict.ts
@@ -2,10 +2,10 @@ import { predictModel } from "AI/utils";
 import { PredictMode } from "enum/enum";
 
 const getDatasetF = async () => {
-    const price = 0.11125;
+    const price = 0.124;
     const reward = 0.1267;
     const timestamp = Math.floor(Date.now() / 1000);
-    const momoInfo = [181, 1615, 4, 14];
+    const momoInfo = [290, 3362, 4, 19];
     momoInfo.push(...[timestamp, price, reward]);
     await predictModel(
         momoInfo,
