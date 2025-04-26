ca1774795b0433534f28fdf1164aa2ce39bb271e
Kay Nguyen <kaynguyen.work@gmail.com>
2025-04-27 02:41:09 +0700

    feat: refactor debugAI function to fetch and process auction data from API

--- src/debugs/debugAI.ts
+++ src/debugs/debugAI.ts
@@ -1,31 +1,25 @@
 import { predictAuctionPro } from "@/AI/utils";
+import { AuctionDto } from "@/types/dtos/Auction.dto";
+import { isProAuction } from "@/utilsV2/find/utils";
+import axios from "axios";
 
 const debugAI = async () => {
-    const auction = {
-        id: "bnb_0xE4534fA363016b1BD1E95C20144361cFB7c2d3aC_0",
-        chain: "bnb",
-        auctor: "0xE4534fA363016b1BD1E95C20144361cFB7c2d3aC",
-        startPrice: 266866000000,
-        endPrice: 266866000000,
-        durationDays: 2,
-        index: 0,
-        ids: [],
-        amounts: [],
-        tokenId: 9933,
-        uptime: 1744114261,
-        prototype: 50021,
-        hashrate: 119,
-        lvHashrate: 4326,
-        level: 40,
-        specialty: 3,
-        category: 5,
-        quality: 6,
-        tx: "0x4e39d58a465ad39f69ad6e282476aac81e25667da162edd1c638d4dce44abeff",
-        deleted: null,
-        nowPrice: 266866000000
-    };
-    const price = await predictAuctionPro(auction);
-    console.log(price);
+    const data = await axios.get(
+        "https://nftapi.mobox.io/auction/list/BNB/0xE4534fA363016b1BD1E95C20144361cFB7c2d3aC?sort=-time&page=1&limit=128"
+    );
+    const myAuctions = data.data.list;
+    const proAuction: AuctionDto[] = [];
+    for (const myAuction of myAuctions) {
+        if (isProAuction(myAuction)) {
+            proAuction.push(myAuction);
+        }
+    }
+    for (const myAuction of proAuction) {
+        const prediction = await predictAuctionPro(myAuction);
+        console.log("=========================================");
+        console.log(myAuction.hashrate, myAuction.lvHashrate, myAuction.prototype, myAuction.level);
+        console.log(prediction);
+    }
 };
 
 debugAI();
